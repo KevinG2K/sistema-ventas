0ec82a2a4b87c0cbbe08890ada09098dd087020d
Kevin Gustavo <kg.2k.pl@gmail.com>
2023-11-06 11:40:11 -0400

    estructura del proyecto + constantes globales
    This reverts commit ba7ff9c3cf003b42ff38706e2e292384a5c53393.

---
+++ configuracion/global.php
@@ -0,0 +1,8 @@
+<?php
+// constantes globales para ser usados en cualquier archivo externo.
+define("DB_HOST", "localhost");
+define("DB_NAME", "sistemaventas");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "123123");
+define("DB_ENCODE", "utf8");
+define("NAME_SYSTEM", "sistema-ventas");
